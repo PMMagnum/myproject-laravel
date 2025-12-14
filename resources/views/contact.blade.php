db94ff12c82f043adbec377677e09315bce743ff
Laresha <lofrancolaresha@gmail.com>
2025-12-14 22:08:01 +0800

    Initial Laravel project with basic routing

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact</title>
+</head>
+<body>
+    <h1>Contact Page</h1>
+    <p>You can contact us here.</p>
+
+    <a href="/">Home</a> |
+    <a href="/about">About</a>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <h1>Homepage</h1>
+    <p>Welcome to my Laravel homepage.</p>
+
+    <a href="/about">About</a> |
+    <a href="/contact">Contact</a>
+</body>
+</html>
